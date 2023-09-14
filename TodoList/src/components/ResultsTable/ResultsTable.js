e5c5af8aae77d9e689a869672e7b6d1d4abf3005
G Teja <130985827+spidey2779@users.noreply.github.com>
2023-09-14 22:36:37 +0530

    Add files via upload

---
+++ TodoList/src/components/ResultsTable/ResultsTable.js
@@ -0,0 +1,35 @@
+import classes from './ResultTable.module.css'
+const formatter =new Intl.NumberFormat('en-US',{
+  style: 'currency',
+  currency: 'USD',
+  minimunFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
+const ResultsTable=(props)=>{
+    return(
+        <table className={classes.result}>
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Total Savings</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.data.map((yeardata)=>(
+            <tr key={yeardata.year}>
+            <td>{yeardata.year}</td>
+            <td>{formatter.format(yeardata.savingsEndOfYear)}</td>
+            <td>{formatter.format(yeardata.yearlyInterest)}</td>
+            <td>{formatter.format(yeardata.savingsEndOfYear - props.initialInvestment - yeardata.yearlyContribution * yeardata.year)}</td>
+            <td>{formatter.format(props.initialInvestment + yeardata.yearlyContribution * yeardata.year)}</td>
+          </tr>
+          ))}
+          
+        </tbody>
+      </table>
+    )
+}
+export default ResultsTable;
