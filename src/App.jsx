eb4c9c3f70ecd16c84294f7575a2feda50d9f8dd
spidey2779 <gteja2779@gmail.com>
2024-06-11 18:22:53 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,84 @@
-import { useState } from "react";
-import Comment from "./component/Comment";
-import useNode from "./hooks/useNode";
-const comments = {
-  id: 1,
-  items: [
-    // {
-    //   id: 1677252427307,
-    //   name: "hello",
-    //   items: [
-    //     {
-    //       id: 1677252434572,
-    //       name: "hello world",
-    //       items: [{ id: 1677252449713, name: "hello world 123", items: [] }],
-    //     },
-    //   ],
-    // },
-    // {
-    //   id: 1677252457839,
-    //   name: "react js",
-    //   items: [{ id: 1677252468098, name: "javascript", items: [] }],
-    // },
-  ],
-};
-
+const menuData = [
+  {
+    label: "Menu 1",
+  },
+  {
+    label: "Menu 2",
+    submenu: [
+      {
+        label: "submenu 1",
+      },
+      {
+        label: "submenu 2",
+      },
+    ],
+  },
+  {
+    label: "Menu 3",
+    submenu: [
+      {
+        label: "submenu 1",
+      },
+      {
+        label: "submenu 2",
+      },
+      {
+        label: "submenu 3",
+      },
+      {
+        label: "submenu 4",
+        submenu: [{ label: "sub submenu1" }, { label: "sub submenu2" }],
+      },
+    ],
+  },
+  {
+    label: "Menu 4",
+    submenu: [
+      {
+        label: "submenu 1",
+      },
+      {
+        label: "submenu 2",
+      },
+    ],
+  },
+];
 const App = () => {
-  const [commentsData, setCommentsData] = useState(comments);
-  const { insertNode, editNode, deleteNode } = useNode();
-  const handleInsertNode = (folderId, item) => {
-    const finalStructrue = insertNode(commentsData, folderId, item);
-    setCommentsData(finalStructrue);
+  const toggleSubmenu = (e) => {
+    let submenu = e.target.querySelector("ul");
+    e.stopPropagation();
+    // console.log(submenu);
+    if (!submenu) return;
+    if (submenu.style.display === "none" || !submenu.style.display) {
+      submenu.style.display = "block";
+    } else {
+      submenu.style.display = "none";
+    }
   };
-  const handleEditNode = (folderId, item) => {
-    const finalStructrue = editNode(commentsData, folderId, item);
-    setCommentsData(finalStructrue);
-  };
-  const handleDeleteNode = (folderId) => {
-    const finalStructrue = deleteNode(commentsData, folderId);
-    const temp = { ...finalStructrue };
-    setCommentsData(temp);
+  const renderSubmenu = (subMenu) => {
+    return (
+      <ul className="p-2  m-2 list-disc list-outside hidden">
+        {subMenu.map((subItem, index) => (
+          <li key={index} onClick={toggleSubmenu}>
+            {" "}
+            {subItem.label}
+            {subItem.submenu && renderSubmenu(subItem.submenu)}
+          </li>
+        ))}
+      </ul>
+    );
   };
   return (
     <div>
-      <Comment comment={commentsData}
-      handleDeleteNode={handleDeleteNode}
-      handleEditNode={handleEditNode}
-      handleInsertNode={handleInsertNode}
-      />
+      <ul className="p-3 m-10 list-disc list-outside">
+        {menuData.map((item, index) => (
+          <li key={index} onClick={toggleSubmenu}>
+            {" "}
+            {item.label}
+            {item.submenu && renderSubmenu(item.submenu)}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/component/Action.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-
-const Action = ({handleClick , type , className}) => {
-  return (
-    <div className={className} onClick={handleClick}>{type}</div>
-  )
-}
-
-export default Action
